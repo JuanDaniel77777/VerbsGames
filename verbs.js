ce7be7ffc72af7be7f37752c750060c65d1db27c
Daniel Ibarra <juandanielibarramendoza@gmail.com>
2022-09-30 18:33:25 -0500

    Avance del poryecto Verbs 30/09/2022

---
+++ verbs.js
@@ -0,0 +1,31 @@
+const verbs=[
+    'open',
+    'close',
+    'walk',
+    'love',
+    'accept',
+    'date',
+    'cook',
+    'listen',
+    'close',
+    'close',
+    'close',
+    'close',
+
+]
+
+const verbos=[
+    'abrir',
+    'cerrar',
+    'caminar',
+    'amar',
+    'aceptar',
+    'citar',
+    'cocinar',
+    'escuchar',
+    'cerrar',
+    'cerrar',
+    'cerrar',
+    'cerrar',
+    'cerrar',
+]
